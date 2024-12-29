58c9ca346aa2c46cbd92f4adee5fc44718d68a57
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2024-12-30 01:17:06 +0300

    Update p.py

--- p.py
+++ p.py
@@ -3,35 +3,36 @@ import HAL
 import cv2
 import numpy as np
 
-p = 0.01
-prev_error = 0
+p = 0.005 #пропорциональный коэффициент ПИД-регулятора
 
 while True:
-    image = HAL.getImage()
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image = HAL.getImage() #получение изображения с камеры
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) #преобразование цветового пространства из BGR в HSV
+    #нижняя и верхняя границы для маскирования цвета
     lower_thresh = np.array([8, 8, 8])
     upper_thresh = np.array([1, 1, 360])
 
-    #console.print("Running")
-    mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
-    mask = cv2.bitwise_not(mask)
+    console.print("Running")
+    mask = cv2.inRange(hsv, lower_thresh, upper_thresh) #маска, выделяющая заданный диапазон цветов
+    mask = cv2.bitwise_not(mask) #инвертация маски
 
-    h, w, d = image.shape
+    h, w, d = image.shape #размеры изображения
+
+    #верхняя и нижняя границы области поиска
     search_top = 3*h/4
     search_bot = search_top + 20
 
-    M = cv2.moments(mask)
-    if M['m00'] != 0:
+    M = cv2.moments(mask) #моменты маски для нахождения центра
+    if M['m00'] != 0: #если линия найдена
+        #координаты центра линии
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
-        cv2.circle(image, (cx, cy), 20, (0, 0, 255), -1)
+        cv2.circle(image, (cx, cy), 20, (0, 0, 255), -1) #круг в центре линии на изображении 
 
-        err = cx - w/2
+        err = cx - w/2 #ошибка (расстояние центра линии от центра изображения)
         console.print(err)
 
-        GUI.showImage(image)
+        GUI.showImage(image) #отображение изображения с нарисованным кругом в центре линии
 
-        HAL.motors.sendV(4)
-        HAL.motors.sendW(p*float(err))
-
-        prev_error = float(err)
+        HAL.motors.sendV(4) #скорость движения 
+        HAL.motors.sendW(p*float(err)) #угловая скорость
